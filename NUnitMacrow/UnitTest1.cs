7d59873ffdcef44786cc0109e4f4f3c6ec42498e
Eckchart <51022063+Eckchart@users.noreply.github.com>
2024-04-22 21:01:20 +0300

    initial commit; working with NUnit for unit testing

---
+++ NUnitMacrow/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitMacrow
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
